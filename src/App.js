90089211d2634910b3b64b6f183c9d628c663626
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-07-01 18:19:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Input from './Components/Input';
-
 import React, { Component } from 'react'
 
 export default class App extends Component {
@@ -12,64 +11,83 @@ export default class App extends Component {
     input4: ""
   }
 
-  moveCursorForward = (fromTextBox, toTextBox) => {
-    console.log("calling.....", fromTextBox.length)
-      if(fromTextBox.length === 4){
-        document.getElementById(toTextBox).focus();
-      }
-  }
-
   handleInput1 = (e) => {
     var value = e.target.value;
-    console.log(value.length);
-    this.setState({
-      input1: value
-    })
+    if(value.length === 16) {
+      var number = value.match(/.{1,4}/g);
+      this.setState({
+        input1: number[0],
+        input2: number[1],
+        input3: number[3],
+        input4: number[3]
+      })
+      this.forthCard.focus();
+    }else if(value.length <= 4) {
+      this.setState({
+        input1: value
+      })
+      if(value.length === 4) {
+        this.secondCard.focus();
+      }
+    }else{
+      return false;
+    }
+    
   }
 
   handleInput2 = (e) => {
     let value = e.target.value;
-    console.log(value);
+    if(value.length > 4){
+      return false;
+    }
     this.setState({
       input2: value
     })
+    if(value.length === 4) {
+      this.thirdCard.focus();
+    }
+    if(value.length < 1){
+      this.firstCard.focus();
+    }
   }
 
   handleInput3 = (e) => {
     let value = e.target.value;
-    console.log(value);
+    if(value.length > 4){
+      return false;
+    }
     this.setState({
       input3: value
     })
+    if(value.length === 4) {
+      this.forthCard.focus();
+    }
+    if(value.length < 1){
+      this.secondCard.focus();
+    }
   }
 
   handleInput4 = (e) => {
     let value = e.target.value;
-    console.log(value);
+    if(value.length > 4) {
+      return false
+    }
     this.setState({
       input4: value
     })
-  }
-
-  moveCursorBackward = (event, value, id) => {
-    console.log("djjd",event.keyCode);
-    console.log("LENGRH>>",value.length)
-    if(event.keyCode === 8 && value.length === 0){
-      console.log("calling movecursor")
-      document.getElementById(id).focus();   
+    if(value.length < 1){
+      this.thirdCard.focus();
     }
   }
-
-
 
   render() {
     return (
       <div className="parent_input">
         <span>Card Number*</span>
-        <Input id="txt1" value={this.state.input1} onKeyDown={(event) => {this.moveCursorBackward(event, this.state.input1, "txt1")}} onChange={(e) => {this.handleInput1(e)}} onKeyUp={() => {this.moveCursorForward(this.state.input1,"txt2")}}/>
-        <Input id="txt2" value={this.state.input2} onKeyDown={(event) => {this.moveCursorBackward(event, this.state.input2, "txt1")}} onChange={(e) => {this.handleInput2(e)}} onKeyUp={() => {this.moveCursorForward(this.state.input2,"txt3")}}/>
-        <Input id="txt3" value={this.state.input3} onKeyDown={(event) => {this.moveCursorBackward(event, this.state.input3, "txt2")}} onChange={(e) => {this.handleInput3(e)}} onKeyUp={() => {this.moveCursorForward(this.state.input3,"txt4")}}/>
-        <Input id="txt4" value={this.state.input4} onKeyDown={(event) => {this.moveCursorBackward(event, this.state.input4, "txt3")}} onChange={(e) => {this.handleInput4(e)}}/>
+        <Input reference={(number) => {this.firstCard=number}} id="txt1" value={this.state.input1} onChange={(e) => {this.handleInput1(e)}} />
+        <Input reference={(number) => {this.secondCard=number}} id="txt2" value={this.state.input2} onChange={(e) => {this.handleInput2(e)}} />
+        <Input reference={(number) => {this.thirdCard=number}} id="txt2" value={this.state.input3} onChange={(e) => {this.handleInput3(e)}} />
+        <Input reference={(number) => {this.forthCard=number}} id="txt2" value={this.state.input4} onChange={(e) => {this.handleInput4(e)}}/>
       </div>
     )
   }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Input extends Component {
+
     
     render() {
         console.log(this.props)
         return (
             <div className="input_box">
-                <input onKeyDown={this.props.onKeyDown} type="text" id={this.props.id} value={this.props.value} onChange={this.props.onChange} maxLength="4" onKeyUp={this.props.onKeyUp}></input>
+                <input ref={this.props.reference} type="text" id={this.props.id} value={this.props.value} onChange={this.props.onChange}></input>
             </div>
         )
     }
